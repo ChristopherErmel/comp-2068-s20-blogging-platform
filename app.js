bbd88a69b5e8470f36f86f28315e3c6393e0ca5f
Christopher E <200250446@student.georgianc.on.ca>
2020-06-17 11:25:26 -0400

    Up to Lesson 4.5
    Added mongo
    Added parser
    Added Connection to mongo db
    Added ENV
    Added Asset Pipeline
    Added css

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+//getting our env variables
+require('dotenv').config();
+
 const path = require('path');
 
 //set our views directory
@@ -9,12 +12,38 @@ const path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+//Mongo access
+const mongoose = require('mongoose');
+//env is the enviornment, bd uri is the url var
+mongoose.connect(process.env.DB_URI, {
+    auth: {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS
+    },
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).catch(err => console.error(`Error: ${err}`));
+
+
+//implement body parser
+//needs to be after connection to db AND before our routes!
+//this allows us to interpret our form paramaters
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
 //need to register our routing
 const routes = require('./routes.js');
 //use this routes file as middleware
 //this will use the middelware provided
 //this will forward all infor from the user into the routes folder...
 app.use('/', routes);
+//use registers our middleware
+app.use('/css', express.static('assets/css'));
+app.use('/js', express.static('assets/js'));
+app.use('/images', express.static('assets/images'));
 
 //Start our server
 app.listen(process.env.PORT || 3000, () => console.log(`Listening on port 3000`));
